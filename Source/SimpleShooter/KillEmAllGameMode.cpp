d1009d8984295c7ccaa5a343e54532a30776b738
Mario Vargas <angstrey@hotmail.com>
2021-11-14 22:30:18 -0500

    Restart game after player dead

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -7,5 +7,10 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
     Super::PawnKilled(PawnKilled);
 
-    UE_LOG(LogTemp, Warning, TEXT("A pawn was killed %s"), *PawnKilled->GetName());
+    APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+
+    if (PlayerController != nullptr)
+    {
+        PlayerController->GameHasEnded(nullptr, false);
+    }
 }
